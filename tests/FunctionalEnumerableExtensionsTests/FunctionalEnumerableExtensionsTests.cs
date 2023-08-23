06a935ec5de6145083f232808c7a8e73fbfec071
Ricardo Tondello <rkdtondello@gmail.com>
2023-08-23 21:02:10 +0200

    add new methods

--- tests/FunctionalEnumerableExtensionsTests/FunctionalEnumerableExtensionsTests.cs
+++ tests/FunctionalEnumerableExtensionsTests/FunctionalEnumerableExtensionsTests.cs
@@ -9,7 +9,9 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.EnsureList();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .WithParameterName("enumerable")
+            .WithMessage("is null (Parameter 'enumerable')");
     }
 
     [Fact]
@@ -27,6 +29,7 @@ public class FunctionalEnumerableExtensionsTests
     {
         var input = Enumerable.Range(1, 5);
 
+        // ReSharper disable PossibleMultipleEnumeration
         var result = input.EnsureList();
 
         result.Should().NotBeSameAs(input).And.BeEquivalentTo(input);
@@ -39,7 +42,9 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.EnsureList();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .WithMessage("is null (Parameter 'enumerable')")
+            .And.ParamName.Should().Be("enumerable");
     }
 
     [Fact]
@@ -49,7 +54,9 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.EnsureArray();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .WithParameterName("enumerable")
+            .WithMessage("is null (Parameter 'enumerable')");
     }
 
     [Fact]
@@ -79,7 +86,9 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.EnsureArray();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .WithMessage("is null (Parameter 'enumerable')")
+            .WithParameterName("enumerable");
     }
 
     [Fact]
@@ -89,7 +98,9 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.AsSpan();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .WithParameterName("enumerable")
+            .WithMessage("is null (Parameter 'enumerable')");
     }
 
     [Fact]
@@ -119,6 +130,119 @@ public class FunctionalEnumerableExtensionsTests
 
         Action action = () => input.AsSpan();
 
-        action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("xs");
+        action.Should().Throw<ArgumentNullException>()
+            .And.ParamName.Should().Be("enumerable");
+        action.Should().Throw<ArgumentNullException>()
+            .And.Message.Should().Be("is null (Parameter 'enumerable')");
+    }
+    
+    [Fact]
+    public void CollectNonNulls_WithNullInput_ThrowsArgumentNullException()
+    {
+        // Arrange
+        IEnumerable<object> input = null!;
+
+        // Act
+        Action action = () => input.CollectNonNulls();
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>()
+            .WithParameterName("enumerable")
+            .WithMessage("is null (Parameter 'enumerable')");
+    }
+
+    [Fact]
+    public void CollectNonNulls_WithAllNonNullItems_ReturnsSameItems()
+    {
+        // Arrange
+        IEnumerable<string> input = new List<string> { "a", "b", "c" };
+
+        // Act
+        var result = input.CollectNonNulls();
+
+        // Assert
+        result.Should().BeEquivalentTo("a", "b", "c");
+    }
+
+    [Fact]
+    public void CollectNonNulls_WithSomeNullItems_ReturnsNonNullItemsOnly()
+    {
+        // Arrange
+        IEnumerable<int?> input = new List<int?> { 1, null, 3, null, 5 };
+
+        // Act
+        var result = input.CollectNonNulls();
+
+        // Assert
+        result.Should().BeEquivalentTo(new [] {1, 3, 5});
+    }
+
+    [Fact]
+    public void CollectNonNulls_WithAllNullItems_ReturnsEmptyCollection()
+    {
+        // Arrange
+        IEnumerable<object> input = new List<object> { null!, null!, null! };
+
+        // Act
+        var result = input.CollectNonNulls();
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+    
+    [Fact]
+    public void EnsureHashSet_WithNullInput_ThrowsArgumentNullException()
+    {
+        // Arrange
+        IEnumerable<object> input = null!;
+
+        // Act
+        Action action = () => input.EnsureHashSet();
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>()
+            .WithParameterName("enumerable")
+            .WithMessage("is null (Parameter 'enumerable')");
+    }
+
+    [Fact]
+    public void EnsureHashSet_WithHashSetInput_ReturnsSameHashSet()
+    {
+        // Arrange
+        var input = new HashSet<int> { 1, 2, 3 };
+
+        // Act
+        var result = input.EnsureHashSet();
+
+        // Assert
+        result.Should().BeSameAs(input);
+    }
+
+    [Fact]
+    public void EnsureHashSet_WithListInput_ReturnsNewHashSetWithSameItems()
+    {
+        // Arrange
+        var input = new List<string> { "a", "b", "c" };
+
+        // Act
+        var result = input.EnsureHashSet();
+
+        // Assert
+        result.Should().BeEquivalentTo("a", "b", "c");
+        result.Should().NotBeSameAs(input);
+    }
+
+    [Fact]
+    public void EnsureHashSet_WithEmptyInput_ReturnsEmptyHashSet()
+    {
+        // Arrange
+        // ReSharper disable once CollectionNeverUpdated.Local
+        var input = new List<int>();
+
+        // Act
+        var result = input.EnsureHashSet();
+
+        // Assert
+        result.Should().BeEmpty();
     }
 }
